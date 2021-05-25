52fc3dcd596514951234d02f6fe96f143d05649f
kunallad54 <kunallad5473@gmail.com>
2021-05-25 17:42:12 +0530

    Employee Monthly Salary Calculated (UC-5 Committed)

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,24 +6,30 @@ fullDayHours=8
 halfDayHours=4
 dailyWage=$((wagePerHour * fullDayHours))
 partTimeWage=$((wagePerHour * halfDayHours))
-attendanceCheck=$((RANDOM%3))
-
-case $attendanceCheck in
-	0)
-		echo "The Employee is Absent"
-		echo "The employee salary for the day is zero"
-		;;
-	1)
-		echo "The Employee is Present"
-		echo "The employee salary for the day is $dailyWage"
-		;;
-	2)
-		echo "The Employee is present half day"
-		echo "The Employee salary for half day is $partTimeWage"
-		;;
-	*)
-		echo "Invalid Output"
-		;;
-esac
-
-
+absentCount=0
+presentCount=0
+halfDayPresentCount=0
+#Here month is considered to be of 20 days
+for(( i=1;i<=20;i++ ))
+do
+	attendanceCheck=$((RANDOM%3))
+	case $attendanceCheck in
+		0)
+			absentCount=$((absentCount + 1))
+			;;
+		1)
+			presentCount=$((presentCount + 1))
+			;;
+		2)
+			halfDayPresentCount=$((halfDayPresentCount + 1))
+			;;
+		*)
+			echo "Invalid Output"
+			;;
+	esac
+done
+echo "Total number of days employee was absent : $absentCount"
+echo "Total number of days employee was present for full day : $presentCount"
+echo "Total number of days employee was present for half day : $halfDayPresentCount"
+salaryCalculator=$((presentCount * dailyWage + halfDayPresentCount * partTimeWage))
+echo "The Employee Salary for the month is : $salaryCalculator"
