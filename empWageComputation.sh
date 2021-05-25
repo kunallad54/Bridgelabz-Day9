4e219137029a790e9e3ae7878c4d496f952fc8e9
kunallad54 <kunallad5473@gmail.com>
2021-05-25 14:48:41 +0530

    The Employee wage Calculated (UC-2 committed)

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,17 @@
 #! /bin/bash -x
 
+
+wagePerHour=20
+fullDayHours=8
+dailyWage=$((wagePerHour * fullDayHours))
 attendanceCheck=$((RANDOM%2))
 if [ $attendanceCheck -eq 0 ]
 then
 	echo "The Employee is Absent"
+	echo "The employee salary for the day is zero"
 else
 	echo "The Employee is Present"
+	echo "The employee salary for the day is $dailyWage"
 fi
+
+
