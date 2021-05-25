88e11a74ebdd05785bbfb54082dbaa8e1b063b05
kunallad54 <kunallad5473@gmail.com>
2021-05-25 15:40:39 +0530

    The Employee Attendance and Wage using switch case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,17 +7,23 @@ halfDayHours=4
 dailyWage=$((wagePerHour * fullDayHours))
 partTimeWage=$((wagePerHour * halfDayHours))
 attendanceCheck=$((RANDOM%3))
-if [ $attendanceCheck -eq 0 ]
-then
-	echo "The Employee is Absent"
-	echo "The employee salary for the day is zero"
-elif [ $attendanceCheck -eq 1 ]
-then
-	echo "The Employee is Present"
-	echo "The employee salary for the day is $dailyWage"
-else
-	echo "The Employee is present half day"
-	echo "The Employee salary for half day is $partTimeWage"
-fi
+
+case $attendanceCheck in
+	0)
+		echo "The Employee is Absent"
+		echo "The employee salary for the day is zero"
+		;;
+	1)
+		echo "The Employee is Present"
+		echo "The employee salary for the day is $dailyWage"
+		;;
+	2)
+		echo "The Employee is present half day"
+		echo "The Employee salary for half day is $partTimeWage"
+		;;
+	*)
+		echo "Invalid Output"
+		;;
+esac
 
 
