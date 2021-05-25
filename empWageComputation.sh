597e1ba03ba66e13f651cfabb9b0fd9ad64faba4
kunallad54 <kunallad5473@gmail.com>
2021-05-25 15:22:33 +0530

    The Part Time Employee added (UC-3 committed)

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,15 +3,21 @@
 
 wagePerHour=20
 fullDayHours=8
+halfDayHours=4
 dailyWage=$((wagePerHour * fullDayHours))
-attendanceCheck=$((RANDOM%2))
+partTimeWage=$((wagePerHour * halfDayHours))
+attendanceCheck=$((RANDOM%3))
 if [ $attendanceCheck -eq 0 ]
 then
 	echo "The Employee is Absent"
 	echo "The employee salary for the day is zero"
-else
+elif [ $attendanceCheck -eq 1 ]
+then
 	echo "The Employee is Present"
 	echo "The employee salary for the day is $dailyWage"
+else
+	echo "The Employee is present half day"
+	echo "The Employee salary for half day is $partTimeWage"
 fi
 
 
